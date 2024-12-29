2ec83bcffb73028d087126e2f7df6fcd4854f104
Ngocvu05 <ngocvu.ngoc06@gmail.com>
2024-12-29 14:33:39 +0700

    add cors

--- src/main/java/com/example/demo/controller/PdfController.java
+++ src/main/java/com/example/demo/controller/PdfController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/api")
 public class PdfController {
