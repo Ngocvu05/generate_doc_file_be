a817b8231b4eb782bb137075f72d8546711892db
Ngocvu05 <ngocvu.ngoc06@gmail.com>
2024-12-29 14:05:42 +0700

    change cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("http://127.0.0.1:5500") // Allow this specific origin
+                        .allowedOrigins("*") // Allow this specific origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies if needed
